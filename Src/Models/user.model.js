2ce31f615c67dbb9b11fa5778191d444c8ff985a
c0d3l0v3r <codelover2404@gmail.com~>
2025-07-07 18:07:35 +0530

    User and Video Model

---
+++ Src/Models/user.model.js
@@ -0,0 +1,81 @@
+import mongoose , {Schema} from "mongoose"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+const userSchema = new Schema({
+    username :{
+        type: String,
+        required : true,
+        unique : true,
+        trim : true,
+        index : true,
+        lowercase : true,
+    },
+    fullname :{
+        type: String,
+        required : true,
+        trim : true,
+        index : true,
+        lowercase : true,
+    },
+    avatar : {
+        type : String, // clodinary Url 
+        required : true,
+    },
+    avatar : {
+        type : String, // clodinary Url 
+    },
+    watchHistory : {
+        type : Schema.Types.ObjectId,
+        ref:"Video" // make later
+    },
+    password : {
+        type : String,
+        required : [true , "password is Required"] 
+    },
+    refreshToken : {
+        type : String, 
+    },
+
+},{timestamps : true })
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password"))return next()
+    this.password = bcrypt.hash(this.password,10)   
+})
+
+
+userSchema.methods.checkPassword = async function (password) {
+    await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function (params) {
+    return jwt.sign({
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname
+    },
+    // secret oof the Access Token
+    process.env.ACCESS_TOKEN_SECRET,
+    // Options
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function (params) {
+    return jwt.sign({
+        _id : this._id,
+    },
+    // secret oof the Refresh Token
+    process.env.REFRESH_TOKEN_SECRET,
+    // Options
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
+
+
+export const User = mongoose.model("User",userSchema)
---
+++ Src/Models/video.model.js
@@ -0,0 +1,37 @@
+import mongoose , {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = Schema({
+    videoFile : {
+        type : String,
+        required : true
+    },
+    ThumbNail : {
+        type : String,
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    views : {
+        type : Number,
+        default :0
+    },
+    isPublished : {
+        type : Boolean,
+        default : true
+    },
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User" 
+    }
+})
+// For writing the aggregate queries on MongoDB we use the Aggregation Framework 
+// mongoose-aggregate-paginate-v2
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",)
+
